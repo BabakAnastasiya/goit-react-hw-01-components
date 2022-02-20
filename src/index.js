841dfe68f332fcd93c1b215ff2c4664a2c393caa
Babak Anastasiya <dp080791bap@gmail.com>
2022-02-20 11:58:48 +0200

    1 task

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from 'components/App';
-import reportWebVitals from './reportWebVitals';
+import { App } from 'components/App'
 import './index.css';
+import profiles from './components/social-profile/user.json'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +11,50 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// const profile = 
+// {
+//   "username": "Jacques Gluke",
+//   "tag": "jgluke",
+//   "location": "Ocho Rios, Jamaica",
+//   "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//   "stats": {
+//     "followers": 5603,
+//     "views": 4827,
+//     "likes": 1308
+//   }
+// }
+const profile = profiles;
+
+function Profile (props){
+  console.log(props);
+  return <div class="profile">
+<div class="description">
+  <img
+    src={props.url}
+    alt="User avatar"
+    class="avatar"
+  />
+  <p class="name">{props.username}</p>
+  <p class="tag">{props.tag}</p>
+  <p class="location">{props.location}</p>
+</div>
+
+<ul class="stats">
+  <li>
+    <span class="label">Followers : </span>
+    <span class="quantity">{props.followers}</span>
+  </li>
+  <li>
+    <span class="label">Views : </span>
+    <span class="quantity">{props.views}</span>
+  </li>
+  <li>
+    <span class="label">Likes : </span>
+    <span class="quantity">{props.likes}</span>
+  </li>
+</ul>
+</div>}
+
+
+ReactDOM.render(<Profile url={profile.avatar} username={profile.username} tag={profile.tag} location={profile.location} followers={profile.stats.followers} views ={profile.stats.views} likes ={profile.stats.likes} />, document.getElementById('root'),);
